d0901f314fe6d646db297a8f550c80eb938b232d
langzhiwu <yaolongxing@163.com>
2018-06-27 17:10:19 +0800

    Create redisclient.go

---
+++ redisclient.go
@@ -0,0 +1,36 @@
+package redislock
+
+import (
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+var (
+	// 定义常量
+	RedisClient *redis.Pool
+	REDIS_HOST  string
+	REDIS_DB    int
+)
+
+func init() {
+	// 从配置文件获取redis的ip以及db
+	REDIS_HOST = "127.0.0.1:6379"
+	//REDIS_DB, _ = 6379
+	// 建立连接池
+	RedisClient = &redis.Pool{
+		// 从配置文件获取maxidle以及maxactive，取不到则用后面的默认值
+		MaxIdle:     100,
+		MaxActive:   100,
+		IdleTimeout: 180 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", REDIS_HOST)
+			if err != nil {
+				return nil, err
+			}
+			// 选择db
+			//c.Do("SELECT", REDIS_DB)
+			return c, nil
+		},
+	}
+}
